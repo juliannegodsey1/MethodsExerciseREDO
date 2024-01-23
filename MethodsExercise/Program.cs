a9696cf76e41b9f06c46968fe36ca1cf4b6d8021
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-01-23 12:40:20 -0500

    initial commit, partially done

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace MethodsExercise
+﻿using System.ComponentModel;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            int sum = Add(2, 3);
+            Console.WriteLine($"The sum is {sum}");
+            Console.WriteLine("-------");
+            Console.WriteLine("");
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {name}! What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is a cool color! What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine($"I like {animal} too! What is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine($"Cool! Here is your profile:");
+            Console.WriteLine("");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine("");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine("");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine("");
+            Console.WriteLine($"Favorite Band: {band}");
+            Console.WriteLine("");
+            Console.WriteLine($"Bye, {name}!");
+            Console.WriteLine("");
+        
+           
+
+        }
+
+        public static int Add(int x,int y)
+        {
+            return x + y;
+
         }
     }
 }
