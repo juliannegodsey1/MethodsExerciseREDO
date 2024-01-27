6626654265f09d408e7c199ff11719bdcaf9c18b
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-01-27 13:37:50 -0500

    finished redo

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,29 +1,126 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MethodsExercise
 {
     public class Program
     {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static int Modulus(int x, int y)
+        {
+            
+            if(y == 0)
+            {
+                throw new ArgumentException("Cannot divide by zero");
+            }
+            
+            return x % y;
+        }
+
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Please give a number:");
+            int num1 = int.Parse(Console.ReadLine());
 
-            int sum = Add(2, 3);
+            Console.WriteLine("");
+
+            Console.WriteLine("Give another number to add to the first number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            var sum = Add(num1, num2);
+
             Console.WriteLine($"The sum is {sum}");
+            Console.WriteLine("");
             Console.WriteLine("-------");
             Console.WriteLine("");
 
+            Console.WriteLine("Please give a number:");
+            int numX = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Give another number to multiply by the first number:");
+            int numY = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            var product = Multiply(numX, numY);
+            
+            Console.WriteLine($"The product of your two numbers is {product}");
+            Console.WriteLine("");
+            Console.WriteLine("-------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Please give a number:");
+            int numA = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Give another number to subtract from the first number:");
+            int numB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            var difference = Subtract(numA, numB);
+
+            Console.WriteLine($"The difference is {difference}");
+            Console.WriteLine("");
+            Console.WriteLine("-------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Please give a number:");
+            int numC = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Give another number to divide by the first number:");
+            int numD = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            var remainder = Modulus(numC, numD);
+
+            Console.WriteLine($"The remainder after dividing {numC} and {numD} is {remainder}");
+            Console.WriteLine("");
+            Console.WriteLine("-------");
+            Console.WriteLine("");
+
+
             Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
+            var name = Console.ReadLine();
 
             Console.WriteLine($"Hi, {name}! What is your favorite color?");
-            string color = Console.ReadLine();
+            var color = Console.ReadLine();
 
             Console.WriteLine($"{color} is a cool color! What is your favorite animal?");
-            string animal = Console.ReadLine();
+            var animal = Console.ReadLine();
 
             Console.WriteLine($"I like {animal} too! What is your favorite band?");
-            string band = Console.ReadLine();
+            var band = Console.ReadLine();
             Console.WriteLine("");
             Console.WriteLine($"Cool! Here is your profile:");
             Console.WriteLine("");
@@ -42,10 +139,6 @@ namespace MethodsExercise
 
         }
 
-        public static int Add(int x,int y)
-        {
-            return x + y;
-
-        }
+       
     }
 }
